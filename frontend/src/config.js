2472560ca5b373ae20f6d6337007ade8948eb584
Ishiita Pal <54171798+palishiita@users.noreply.github.com>
2024-06-13 08:38:44 +0200

    Delete frontend directory

--- frontend/src/config.js
+++
@@ -1,9 +0,0 @@
-import { Amplify } from 'aws-amplify';
-
-Amplify.configure({
-    Auth: {
-        region: 'us-east-1',
-        userPoolId: 'us-east-1_t4Bm131XJ',
-        userPoolWebClientId: '5em51fivqp96eqrmegkgllfvv3'
-    }
-});
